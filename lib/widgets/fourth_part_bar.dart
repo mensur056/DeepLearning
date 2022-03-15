d729ba4aa7bdce3df02779cd42ec1c704ca3c710
mensur056 <mansur.sarxanov@gmail.com>
2022-03-15 09:59:37 +0100

    DeepLearning

--- lib/widgets/fourth_part_bar.dart
+++ lib/widgets/fourth_part_bar.dart
@@ -34,6 +34,47 @@ class FourthPartBar extends StatelessWidget {
                   borderRadius: BorderRadius.all(Radius.circular(15))),
               width: 1000,
               height: 500,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(40),
+                        child: Image.asset(
+                          'images/Workera.webp',
+                          height: 220,
+                          width: 350,
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: const [
+                            Text(
+                              'Don’t know where to start? Test your current abilities and create your personalized learning pathway to become a machine learning expert.',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            Text(
+                              'Already an ML practitioner? Make sure that you’re up to date with the latest techniques and advance your career by identifying your next steps.',
+                              style: TextStyle(color: Colors.black),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const Divider(
+                    height: 1,
+                    color: Colors.black,
+                  ),
+                  const Text(
+                      'Workera’s free assessments help you identify the skills you need for the AI roles you want, providing the feedback, resources, and credentials to successfully showcase your skillset.'),
+                  Container(
+                    height: 60,
+                    width: 180,
+                    color: Colors.red,
+                  )
+                ],
+              ),
             ),
           ),
           Expanded(
